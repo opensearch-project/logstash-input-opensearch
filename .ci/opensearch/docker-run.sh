ee7d408b019f373d54d7e7e5063b43efa04c744d
Naveen Tatikonda <navtat@amazon.com>
2022-03-09 10:23:32 -0600

    Add opensearch to CI and update build scripts
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>

---
+++ .ci/opensearch/docker-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is intended to be run inside the docker container as the command of the docker-compose.
+set -ex
+
+cd .ci/opensearch
+
+docker-compose up --exit-code-from logstash
